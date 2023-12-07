5ea43567d0c2648fbb24656c6436e0f75d19c7a6
Odell McFarland IV <odell.mac4@gmail.com>
2023-12-07 00:51:40 -0800

    Feat: create venue class with methods to add, yell at, and kick out patrons

--- venue/lib/venue.rb
+++ venue/lib/venue.rb
@@ -1,3 +1,28 @@
 class Venue
-    def
+
+    attr_reader :name, :capacity, :patrons
+
+    def initialize(name , capacity)
+        @name = name
+        @capacity = capacity
+        @patrons = []
+    end
+
+    def add_patron(name)
+        patrons << name
+    end
+
+    def yell_at_patrons
+        patrons.map {|patron| patron.upcase}
+    end
+
+    def over_capacity?
+        patrons.length  > capacity
+    end
+
+    def kick_out
+        patrons.slice!(patrons.length - 1)
+        # patrons.delete_at(patrons.length - 1)
+    end
+    
 end
